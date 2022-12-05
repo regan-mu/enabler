80cba77193594e5bb2ec1a7074c55fb398b7732e
Regan254 <40056428+regan254@users.noreply.github.com>
2022-12-05 10:45:05 +0300

    Fix Routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="game">
       <Routes>
-        <Route path="/enabler" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/signup" element={<Signup />} /> 
       </Routes>
